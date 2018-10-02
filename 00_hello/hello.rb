6ab8044fc88de5c4bce589beb6100f3c777ffe35
Rene Solarte <renesolarte@gmail.com>
2018-10-02 23:19:11 +0000

    First Commit

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,16 @@
 #write your code here
+def hello (name)
+    if name == "Rene"
+        puts "Hello #{name}"
+    else
+        puts "Hello World!"
+    end
+end
+
+def main
+    print "What's your name ?"
+    name= gets.chomp
+    hello(name)
+end
+
+main if __FILE__==$PROGRAM_NAME
