f378ed2664510c9e89aa83ee8d60b9b76cbe60f1
Rene Solarte <renesolarte@gmail.com>
2018-10-03 23:32:17 +0000

    Initial Commit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,18 +1,21 @@
 #write your code here
 def temperature(tempf)
     tempc = (tempf - 32) * (0.5555555)
+    puts "#{tempf} °F = #{tempc} °C"
 end
-def boiling (tempf)
-    bolf = 100
+def boiling
+    puts "Boiling Temperature in °C = 100"
+    puts "Boiling Temperature in °F = 212"
 end
-
-
+def freezing
+    puts "Freezing Temperature in °C = 0"
+    puts "Freezing Temperature in °F = 32"
+end
 def main
-    print "Insert Temperature (F) :"
+    print "Insert Temperature in (°F) :"
     tempf= gets.chomp
     temperature(tempf.to_f)
-    puts "Temperature in (C) = #{temperature(tempf.to_f)}"
-    boiling (tempf)
-    puts "Boiling Temperature in (C) = #{boiling(tempf.to_f)}"
+    boiling
+    freezing
 end
 main if __FILE__==$PROGRAM_NAME
