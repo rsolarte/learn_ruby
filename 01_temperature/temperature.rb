4b1845f835e52758b5b06e5c43e5ef4bfb52fc54
Rene Solarte <renesolarte@gmail.com>
2018-10-03 00:35:30 +0000

    first commit

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,18 @@
 #write your code here
+def temperature(tempf)
+    tempc = (tempf - 32) * (0.5555555)
+end
+def boiling (tempf)
+    bolf = 100
+end
+
+
+def main
+    print "Insert Temperature (F) :"
+    tempf= gets.chomp
+    temperature(tempf.to_f)
+    puts "Temperature in (C) = #{temperature(tempf.to_f)}"
+    boiling (tempf)
+    puts "Boiling Temperature in (C) = #{boiling(tempf.to_f)}"
+end
+main if __FILE__==$PROGRAM_NAME
